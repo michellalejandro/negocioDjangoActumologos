ef6861be7f4ac187496fd282369227c21a5f5e52
Michell Lopez <ingmichell5101@gmail.com>
2023-08-12 14:00:36 -0600

    First template

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os;
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Mi negocio web<h1>")
+    return render(request,"core/index.html")
