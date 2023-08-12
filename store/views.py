15488285992ba4e4cca6b9cf6f512ed085785f5f
Michell Lopez <ingmichell5101@gmail.com>
2023-08-12 13:18:10 -0600

    url patters added

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
+# Create your views here.
 def index(request):
-    return HttpResponse("<h1>Mi negocio web<h1>")
+    return HttpResponse("<h1>Bienvenido a la tienda<h1>")
