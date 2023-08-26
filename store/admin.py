f21b07886a273961aa0a6382fdaf666d0e774984
Michell Lopez <ingmichell5101@gmail.com>
2023-08-25 21:08:16 -0600

    Primera categoria y producto agregados

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+#Agregamos los modelos en el panel de administracion de Django
+admin.site.register(Categoria)
+admin.site.register(Producto)
+
